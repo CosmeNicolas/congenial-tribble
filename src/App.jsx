e5ef1695e769528e28282d0ad4aaa202e15fef5d
CosmeNicolas <nicomas7590@gmail.com>
2023-12-29 14:08:26 -0300

    le di formato al proyecto y estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+import { Container } from "react-bootstrap"
+import Footer from "./components/Footer"
 
 function App() {
 
 
+
   return (
     <>
-     
+    <Container className="main">
+     <h1 className="text-center">Hola mundo </h1>
+    </Container>
+    <Footer/>
     </>
   )
 }
